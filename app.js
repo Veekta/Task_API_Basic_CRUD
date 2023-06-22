5e9ab2c549f0e74a04a53aff3f6d59ca0dd12502
veekta <vsunday.1710@gmail.com>
2023-06-22 04:26:39 +0100

    change to production mode

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tasks = require("./routes/tasks.js");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const connectDB = require("./db/connect");
 require("dotenv").config();
